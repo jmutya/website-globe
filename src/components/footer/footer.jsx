b347c138f1841d5eb9692368273ad091f2f17cdd
jmutya <j.mutya.477708@umindanao.edu.ph>
2025-03-21 17:25:41 +0800

    social media added

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -8,19 +8,42 @@ const Footer = () => {
         <div className="footer-about">
           <h2>About Us</h2>
           <p>
-            We are committed to providing the best services and creating solutions that make a difference.
+            We are committed to providing the best services and creating
+            solutions that make a difference.
           </p>
         </div>
         <div className="footer-social">
           <h2>Follow Us</h2>
           <div className="social-links">
-            <a href="#" className="social-icon">🔵</a>
-            <a href="#" className="social-icon">🐦</a>
-            <a href="#" className="social-icon">📸</a>
+            <a
+              href="https://www.facebook.com/globeph"
+              className="social-icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a
+              href="https://x.com/enjoyGLOBE"
+              className="social-icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-x-twitter"></i>
+            </a>
+
+            <a
+              href="https://www.instagram.com/enjoyglobe"
+              className="social-icon"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
           </div>
         </div>
       </div>
-      <p className="footer-bottom">&copy; 2025 Globe Telecom INC. All rights reserved.</p>
+      <p className="footer-bottom">
+        &copy; 2025 Globe Telecom INC. All rights reserved.
+      </p>
     </footer>
   );
 };
